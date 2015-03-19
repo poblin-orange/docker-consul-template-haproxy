93a05043d51504b9cde11f83f5365e44d3b43770
shayashibara <shayashibara@trifort.jp>
2015-03-19 14:59:23 +0900

    Improved startup script

--- startup.sh
+++ startup.sh
@@ -3,12 +3,16 @@
 HAPROXY="/etc/haproxy"
 PIDFILE="/var/run/haproxy.pid"
 CONFIG_FILE=${HAPROXY}/haproxy.cfg
+TEMPLATE=${HAPROXY}/haproxy.template
 
 cd "$HAPROXY"
 
 haproxy -f "$CONFIG_FILE" -p "$PIDFILE" -D -st $(cat $PIDFILE)
 
+current=$(curl -X GET ${CONSUL_PORT_8500_TCP_ADDR:-172.17.42.1}:${CONSUL_PORT_8500_TCP_PORT:-8500}/v1/kv/backend/current)
+[[ -z "$current" ]] && sed -i -e "s/default_backend.*$/default_backend default/" $TEMPLATE
+
 env
 
 /usr/local/bin/consul-template -consul ${CONSUL_PORT_8500_TCP_ADDR:-172.17.42.1}:${CONSUL_PORT_8500_TCP_PORT:-8500} \
-  -template "/etc/haproxy/haproxy.template:/etc/haproxy/haproxy.cfg:/hap.sh"
+	  -template "/etc/haproxy/haproxy.template:/etc/haproxy/haproxy.cfg:/hap.sh"
