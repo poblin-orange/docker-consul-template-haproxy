a8974ab179094eaf6abcc5c02cbda62d0ee6e68a
Pierre Oblin <pierre.oblin@orange.com>
2015-10-09 14:11:50 +0200

    use env var BACKEND_SERVICE remove tag filtering

--- startup.sh
+++ startup.sh
@@ -4,11 +4,11 @@ HAPROXY="/etc/haproxy"
 PIDFILE="/var/run/haproxy.pid"
 CONFIG_FILE=${HAPROXY}/haproxy.cfg
 TEMPLATE=${HAPROXY}/haproxy.template
-SERVICE_TAG=${SERVICE_TAG:webapp}
+$BACKEND_SERVICE=${$BACKEND_SERVICE:webapp}
 
 cd "$HAPROXY"
 
-sed -i -e "s/webapp/${SERVICE_TAG}/g" $TEMPLATE
+sed -i -e "s/webapp/${$BACKEND_SERVICE}/g" $TEMPLATE
 
 haproxy -f "$CONFIG_FILE" -p "$PIDFILE" -D -st $(cat $PIDFILE)
 
